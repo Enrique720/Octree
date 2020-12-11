fb00a696e7b19c893e403c8af230f89c34c0b169
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-12-11 16:45:47 -0500

    main

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 CImg<char> Binarizar(CImg<float> & img, int umbral)
 {
-    CImg<char> R(img.width(),img.height());
-    for(int i=0;i< img.width();i++)
+    CImg<char> R(img.width(), img.height());
+    for(int i=0;i<img.width();i++)
         for(int j=0;j< img.height();j++)
         {
             int r = img(i,j,0);
@@ -26,13 +26,14 @@ CImg<char> Binarizar(CImg<float> & img, int umbral)
 
 CImg<float> load_3d(){
     int x = 512, y = 512, z = 40;
-    CImg<char> R(x,y,z,3);
+    CImg<float> R(x,y,z,3);
     ifstream db("db.txt");
     string filename;
     for(int k = 0; k < R.depth(); k++){
         getline(db,filename);
         string temp = "datos cerebros/paciente 1/1/" + filename;
         CImg<float> img (temp.c_str());
+        //img.display();
         for(int i = 0; i < R.height(); i++){
             for(int j = 0; j < R.width(); j++){
                 R(i,j,k,0) = img(i,j,0);
@@ -46,15 +47,12 @@ CImg<float> load_3d(){
 
 
 int main(){
-   
     srand(time(NULL));   
-   // A.save("in.jpg");
+    //A.save("in.jpg");
     //CImg<float> B = A.crop(0, 0, 3, 3); 
     //CImg<char> R = Binarizar(A,40);
     load_3d().display();
-   // A.display();
-    //R.display();
-
-    
+    //A.display();
+    //R.display();   
     return 0;
 }
