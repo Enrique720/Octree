b0907f78569f47046742d75abe0ab30d92214d97
cesar <camgcamg11@gmail.com>
2020-12-12 17:48:23 -0500

    changed types

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include "octree.h"
 int main(){
-    pixel_des temp = {0, 6, 0, 6, 0, 6};
-    plano input = {2,3,0,2};
+    pixel_des temp = {0, 3, 0, 3, -1, 1.5};
+    plano input = {1,0,0,-4};
     if(intersect(input,temp)){
         cout<<"nice"<<endl;
     }else{
