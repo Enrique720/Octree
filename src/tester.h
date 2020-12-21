2be2841595e11c0fd3ec25d5704877838e1979c0
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-12-20 21:17:55 -0500

    tester

--- src/tester.h
+++ src/tester.h
@@ -3,6 +3,7 @@
 void tester(Octree &oct, CImg<unsigned char> &R) {
     clock_t time1, time2;
     double ang1, ang2;
+    cout << "Tiempo Octree  & Tiempo Cubo " << endl;
     for (int i = 0; i < 20; i++) {
         
         ang1 = (rand() % 3000) / 1000;
@@ -11,17 +12,17 @@ void tester(Octree &oct, CImg<unsigned char> &R) {
         time1 = clock();
         CImg<unsigned char> RR = oct.Get_Cut(R.width(), R.height(), R.depth(), ang1, ang2);
         time2 = clock(); 
-        cout << "Tiempo Octree: " << double(time2-time1)/CLOCKS_PER_SEC << endl;
+        cout << double(time2-time1)/CLOCKS_PER_SEC << " & ";
         
         string filename = "testImg/" + to_string(i) + ".jpg";
-        RR.save(filename.c_str());
+        //RR.save(filename.c_str());
 
         time1 = clock();
         CImg<unsigned char> RR2 = Get_Cut(R.width(), R.height(), R.depth(), ang1, ang2, R);
         time2 = clock();
 
-        cout << "Tiempo Cubo: " << double(time2-time1)/CLOCKS_PER_SEC << endl;
+        cout << double(time2-time1)/CLOCKS_PER_SEC << endl;
         string filename2 = "testImgR/" + to_string(i) + ".jpg";
-        RR2.save(filename2.c_str());
+        //RR2.save(filename2.c_str());
     }
 }
